3d324db209be4e4f8ce01e798527ce3d0dfe3bb7
sunilkumar chandrasekharan <sunil.lmc@gmail.com>
2016-01-01 15:07:46 -0600

    formatted

--- Test/src/com/study/problems/ReverseWordsInAString.java
+++ Test/src/com/study/problems/ReverseWordsInAString.java
@@ -1,42 +1,43 @@
 package com.study.problems;
 
 public class ReverseWordsInAString {
-	
+
 	public static void main(String[] args) {
-		String input =" This is , my car.i love it";
+		String input = " This is , my car.i love it";
 		System.out.println(reverseWords(input));
-		
+
 	}
 
 	private static String reverseWords(String input) {
-		if(input==null || input.trim().equals("")){
+		if (input == null || input.trim().equals("")) {
 			return input;
 		}
 		int counter = 0;
 		int wordStart = 0;
-		char[] inputArr =input.toCharArray();
-		reverse(inputArr,0,inputArr.length-1);
+		char[] inputArr = input.toCharArray();
+		reverse(inputArr, 0, inputArr.length - 1);
 		System.out.println(inputArr);
-		while (counter<inputArr.length){
-			
-			if(inputArr[counter] == ' ' ||inputArr[counter] == ',' || inputArr[counter] == '.' ){
-				reverse(inputArr,wordStart,counter-1);
-				wordStart= counter+1;
+		while (counter < inputArr.length) {
+
+			if (inputArr[counter] == ' ' || inputArr[counter] == ','
+					|| inputArr[counter] == '.') {
+				reverse(inputArr, wordStart, counter - 1);
+				wordStart = counter + 1;
 			}
 			counter++;
 		}
-		reverse(inputArr,wordStart,counter-1);
+		reverse(inputArr, wordStart, counter - 1);
 		return new String(inputArr);
 	}
 
-	private static void reverse(char[] input, int wordStart,int length) {
-		char temp=' ';
+	private static void reverse(char[] input, int wordStart, int length) {
+		char temp = ' ';
 		int i = length;
-		if(wordStart <i){
-			while(wordStart<i){
+		if (wordStart < i) {
+			while (wordStart < i) {
 				temp = input[i];
-				input[i]= input[wordStart];
-				input[wordStart]= temp;
+				input[i] = input[wordStart];
+				input[wordStart] = temp;
 				wordStart++;
 				i--;
 			}
