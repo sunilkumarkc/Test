e10f77256ca5e4356521cf087755c49588ddcdb0
sunilkumar chandrasekharan <sunil.lmc@gmail.com>
2016-01-01 16:25:45 -0600

    fixed bug

--- Test/src/com/study/problems/MergeTwoSortedArrays.java
+++ Test/src/com/study/problems/MergeTwoSortedArrays.java
@@ -1,32 +1,37 @@
 package com.study.problems;
 
 public class MergeTwoSortedArrays {
-	
-	public int[] mergeTwoArrays(int[] first,int[] second,int firstEnd,int secondEnd){
-		int modifiedEnd = firstEnd+secondEnd+1;
-		while (secondEnd>=0){
-			if(second[secondEnd]>=first[firstEnd]){
-				first[modifiedEnd] = second[secondEnd];
-				secondEnd--;
-			}else{
-				first[modifiedEnd] = first[firstEnd];
-				firstEnd--;
+
+	public int[] mergeTwoArrays(int[] first, int[] second, int firstEnd,
+			int secondEnd) {
+		int modifiedEnd = firstEnd + secondEnd - 1;
+		int firstLastIndex = firstEnd - 1;
+		int secondLastIndex = secondEnd - 1;
+		while (secondLastIndex >= 0) {
+			if (firstLastIndex >= 0
+					&& first[firstLastIndex] >= second[secondLastIndex]) {
+				first[modifiedEnd] = first[firstLastIndex];
+				firstLastIndex--;
+				
+			} else {
+				first[modifiedEnd] = second[secondLastIndex];
+				secondLastIndex--;
 			}
 			modifiedEnd--;
 		}
 		return first;
 	}
-	
+
 	public static void main(String[] args) {
 		int[] a = new int[10];
-		a[0] = 1;
-		a[1]=2;
-		a[2]=3;
-		
-		int[] b= {5,6,7};
-		
-		for(int val:new MergeTwoSortedArrays().mergeTwoArrays(a, b, 2, 2)){
-			System.out.print(val+",");
+		a[0] = 7;
+		a[1] = 8;
+		a[2] = 9;
+
+		int[] b = { 5, 6, 7 };
+
+		for (int val : new MergeTwoSortedArrays().mergeTwoArrays(a, b, 3, 3)) {
+			System.out.print(val + ",");
 		}
 	}
 
